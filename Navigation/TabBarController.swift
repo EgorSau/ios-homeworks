a4e53c2ebe6a07c494c9ff83988f2548bac732cd
Egor Saushkin <egor.saushkin@icloud.com>
2022-02-15 21:43:06 +0400

    Navigation HW done

---
+++ Navigation/TabBarController.swift
@@ -0,0 +1,71 @@
+//
+//  TabBarController.swift
+//  Navigation
+//
+//  Created by Egor SAUSHKIN on 14.02.2022.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        enum Index {
+            case none
+            case first
+            case second
+            
+            var title: String? {
+                switch self {
+                case .none:
+                    return nil
+                case .first:
+                    return "Лента пользователя"
+                case .second:
+                    return "Профиль"
+                }
+            }
+            var image: String {
+                switch self {
+                case .none:
+                    return ""
+                case .first:
+                    return "list.bullet.circle.fill"
+                case .second:
+                    return "person.circle"
+                }
+            }
+        }
+
+        var arrayVC: [UIViewController] = [FeedVC(), ProfileVC()]
+        
+        arrayVC[0] = UINavigationController(rootViewController: FeedVC())
+        
+        self.viewControllers = arrayVC.map({ tabBarItem in
+            switch tabBarItem {
+            case FeedVC():
+                return UINavigationController(rootViewController: FeedVC())
+            case ProfileVC():
+                return ProfileVC()
+            default:
+                break
+            }
+            return tabBarItem
+        })
+        
+        arrayVC.enumerated().forEach({ (index, viewController) in
+            switch index {
+            case 0:
+                viewController.tabBarItem.image = UIImage(systemName: Index.first.image)
+                viewController.tabBarItem.title = Index.first.title
+            case 1:
+                viewController.tabBarItem.image = UIImage (systemName: Index.second.image)
+                viewController.tabBarItem.title = Index.second.title
+            default:
+                viewController.tabBarItem.title = Index.none.title
+            }
+        })
+    }
+}
