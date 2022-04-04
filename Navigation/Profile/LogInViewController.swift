3db3944718cdca14508a5438dcb5e4c47daeaa6b
Egor Saushkin <egor.saushkin@icloud.com>
2022-04-04 21:12:10 +0400

    LoginView updated: Main task done

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class LogInViewController: UIViewController {
     
+    let userLogin = "EgorS@mail.com"
+    let userPass = "123456"
+    
     lazy var scrollView: UIScrollView = {
         var scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
@@ -54,6 +57,17 @@ class LogInViewController: UIViewController {
         return textField
     }()
     
+    lazy var warningLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Pass must have 6 characters"
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.isHidden = true
+        label.alpha = 0
+        return label
+    }()
+    
     lazy var stackView: UIStackView = {
         let stack = UIStackView()
         stack.translatesAutoresizingMaskIntoConstraints = false
@@ -95,6 +109,10 @@ class LogInViewController: UIViewController {
     
     deinit {
         self.KbdNotificatorRemove()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
     }
     
     override func viewDidLayoutSubviews() {
@@ -142,6 +160,7 @@ class LogInViewController: UIViewController {
         let leadingConstraint = self.scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor)
         let trailingConstraint = self.scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
         let bottomConstraint = self.scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+
         
         NSLayoutConstraint.activate([topConstraint,
                                      leadingConstraint,
@@ -179,7 +198,8 @@ class LogInViewController: UIViewController {
     }
     
     private func setupButton(){
-        self.view.addSubview(self.logInButton)
+        self.view.addSubview(logInButton)
+        self.view.addSubview(warningLabel)
         self.logInButton.layer.cornerRadius = 10
         self.logInButton.clipsToBounds = true
         let pixel = UIImage(named: "blue_pixel.png")
@@ -193,7 +213,14 @@ class LogInViewController: UIViewController {
             self.logInButton.alpha = 1.0
         }
         
-        let topConstraint = self.logInButton.topAnchor.constraint(equalTo: self.stackView.bottomAnchor, constant: 16)
+        //warning setup
+        let topWarningConstraint = self.warningLabel.topAnchor.constraint(equalTo: self.stackView.bottomAnchor, constant: 5)
+        let trailingWarningConstraint = self.warningLabel.trailingAnchor.constraint(equalTo: self.stackView.trailingAnchor)
+        let leadingWarningConstraint = self.warningLabel.leadingAnchor.constraint(equalTo: self.stackView.leadingAnchor)
+        let heightWarningConstraint = self.warningLabel.heightAnchor.constraint(equalToConstant: 10)
+        
+        //button setup
+        let topConstraint = self.logInButton.topAnchor.constraint(equalTo: self.warningLabel.bottomAnchor, constant: 16)
         let trailingConstraint = self.logInButton.trailingAnchor.constraint(equalTo: self.stackView.trailingAnchor)
         let leadingConstraint = self.logInButton.leadingAnchor.constraint(equalTo: self.stackView.leadingAnchor)
         let heightConstraint = self.logInButton.heightAnchor.constraint(equalToConstant: 50)
@@ -202,11 +229,68 @@ class LogInViewController: UIViewController {
                                      trailingConstraint,
                                      leadingConstraint,
                                      heightConstraint,
+                                     topWarningConstraint,
+                                     trailingWarningConstraint,
+                                     leadingWarningConstraint,
+                                     heightWarningConstraint
                                     ])
     }
     
     @objc private func buttonPressed(){
+        //textFields empty?
+        guard self.emailPhoneTextField.text != "" || self.passTextField.text != "" else {
+            self.shake()
+            return
+        }
+
+        //passField correct?
+        guard self.passTextField.text?.count == 6 else {
+            self.shake()
+            self.warningAppear()
+            return
+        }
+        
+        //log & pass control
+        guard self.emailPhoneTextField.text == userLogin && self.passTextField.text == userPass else {
+            self.showAlert()
+            return
+        }
+        
         let profileVC = ProfileViewController()
         navigationController?.pushViewController(profileVC, animated: true)
     }
+    
+    private func showAlert() {
+        let alert = UIAlertController(title: "Something goes wrong", message: "Login or password incorrect", preferredStyle: .alert)
+        let action = UIAlertAction(title: "ОК", style: .default)
+        present(alert, animated: true, completion: .none)
+        alert.addAction(action)
+    }
 }
+
+protocol Animated {
+    func shake()
+    func warningAppear()
+}
+
+extension LogInViewController: Animated {
+    func shake() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+        
+        animation.isAdditive = true
+        self.passTextField.layer.add(animation, forKey: "shake")
+        self.emailPhoneTextField.layer.add(animation, forKey: "shake")
+    }
+    
+    func warningAppear(){
+        self.warningLabel.isHidden = false
+        UIView.animate(withDuration: 0.6) {
+            self.warningLabel.alpha = 1
+        }
+    }
+}
+
